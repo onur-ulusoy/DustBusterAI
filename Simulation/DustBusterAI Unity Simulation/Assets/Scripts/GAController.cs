84d885198ab3df850a4cdf1363bf687a241bab1f
Onur Ulusoy <onurulusoys4@gmail.com>
2023-02-06 22:48:55 +0300

    tsp algorithm integrated.

---
+++ Simulation/DustBusterAI Unity Simulation/Assets/Scripts/GAController.cs
@@ -0,0 +1,113 @@
+using System.Threading;
+using GeneticSharp.Domain;
+using GeneticSharp.Domain.Crossovers;
+using GeneticSharp.Domain.Mutations;
+using GeneticSharp.Domain.Populations;
+using GeneticSharp.Domain.Selections;
+using GeneticSharp.Domain.Terminations;
+using GeneticSharp.Infrastructure.Framework.Threading;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GAController : MonoBehaviour
+{
+    private GeneticAlgorithm m_ga;
+    private Thread m_gaThread;
+
+    public int m_numberOfCities = 20;
+    public Object CityPrefab;
+
+    private System.Collections.Generic.IList<TspCity> Cities;
+    public List<GameObject> citiesGO;
+
+    private LineRenderer m_lr;
+    private void Awake()
+    {
+        m_lr = GetComponent<LineRenderer>();
+        m_lr.positionCount = m_numberOfCities + 1;
+    }
+    private void Start()
+    {
+        var fitness = new TspFitness(m_numberOfCities);
+        var chromosome = new TspChromosome(m_numberOfCities);
+
+        // This operators are classic genetic algorithm operators that lead to a good solution on TSP,
+        // but you can try others combinations and see what result you get.
+        var crossover = new OrderedCrossover();
+        var mutation = new ReverseSequenceMutation();
+        var selection = new RouletteWheelSelection();
+        var population = new Population(50, 100, chromosome);
+
+        m_ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation);
+        m_ga.Termination = new TimeEvolvingTermination(System.TimeSpan.FromHours(1));
+
+        // The fitness evaluation of whole population will be running on parallel.
+        m_ga.TaskExecutor = new ParallelTaskExecutor
+        {
+            MinThreads = 100,
+            MaxThreads = 200
+        };
+
+        // Everty time a generation ends, we log the best solution.
+        m_ga.GenerationRan += delegate
+        {
+            var distance = ((TspChromosome)m_ga.BestChromosome).Distance;
+            //Debug.Log($"Generation: {m_ga.GenerationsNumber} - Distance: ${distance}");
+        };
+
+        Cities = DrawCities();
+
+        // Starts the genetic algorithm in a separate thread.
+        m_gaThread = new Thread(() => m_ga.Start());
+        m_gaThread.Start();
+    }
+
+    private void OnDestroy()
+    {
+        // When the script is destroyed we stop the genetic algorithm and abort its thread too.
+        m_ga.Stop();
+        m_gaThread.Abort();
+    }
+
+    System.Collections.Generic.IList<TspCity> DrawCities()
+    {
+        var cities = ((TspFitness)m_ga.Fitness).Cities;
+        
+        for (int i = 0; i < m_numberOfCities; i++)
+        {
+            Vector3 pos = new Vector3(i*2f, 2.08f, 0f);
+            var city = cities[i];
+            var go = Instantiate(CityPrefab, pos, Quaternion.identity) as GameObject;
+            go.name = "City " + i;
+            citiesGO.Add(go);
+            city.Position = go.transform.position;
+            go.GetComponent<CityController>().Data = city;
+
+        }
+
+        return cities;
+    }
+
+    void DrawRoute()
+    {
+        var c = m_ga.Population.CurrentGeneration.BestChromosome as TspChromosome;
+
+        if (c != null)
+        {
+            var genes = c.GetGenes();
+
+            for (int i = 0; i < genes.Length; i++)
+            {
+                m_lr.SetPosition(i, citiesGO[i].transform.position);
+            }
+
+            var firstCity = Cities[(int)genes[0].Value];
+            m_lr.SetPosition(m_numberOfCities, firstCity.Position);
+        }
+    }
+
+    private void Update()
+    {
+        DrawRoute();
+    }
+}
